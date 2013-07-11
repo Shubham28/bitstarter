fc88ec6e5e51c3a8df68206e9c2583b5c89ec1fb
Shubham Gupta <ubuntu@ip-172-31-5-195.us-west-2.compute.internal>
2013-07-11 19:24:07 +0000

    Reading from index

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+var indexFile;
+
+fs.readFile("index.html", function (err, data) {
+	if (err)
+		throw err;
+	indexFile = data.toString();
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexFile);
 });
 
 var port = process.env.PORT || 5000;
